b94fc4713ecc7b8478e0f2492b9c95166332bb79
mxsaa7 <84366332+mxsaa7@users.noreply.github.com>
2022-05-07 10:26:40 -0400

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = "api_key=adf1eddf1e88696a14f21f88d3520e85";
+const API_KEY = "api_key=YOUR_TMDB_API_KEY";
 const API_URL = BASE_URL + "/discover/movie?" + queryDate() + "&" + API_KEY;
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
 
